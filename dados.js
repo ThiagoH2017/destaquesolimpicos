f100567b33d87b672e0b4c0144eedbf4d26bd25f
ThiagoH2017 <30364200+ThiagoH2017@users.noreply.github.com>
2024-09-09 23:49:59 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+      titulo: "Rayssa Leal",
+      descricao: "Jhulia Rayssa Mendes Leal é uma skatista brasileira, vice-campeã olímpica nos Jogos Olímpicos de Verão de 2020 em Tóquio, sendo a mais jovem medalhista olímpica brasileira. Em 2024, conquistou o bronze nos Jogos Olímpicos de Paris.",
+      link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+      tags: "skate fada sk8"
+    },
+    {
+      titulo: "Rebeca Andrade",
+      descricao: "Rebeca Andrade é uma ginasta brasileira, campeã olímpica nos Jogos Olímpicos de Verão de 2020 em Tóquio, conquistando a medalha de ouro no individual geral. Em 2024, conquistou o prata nos Jogos Olímpicos de Paris.",
+      link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+      tags:  "ginástica ginastica ouro"
+    },
+    {
+      titulo: "Beatriz Souza",
+      descricao: "Beatriz Souza é uma judoca brasileira, medalhista de bronze nos Jogos Olímpicos de Verão de 2020 em Tóquio. Em 2024, conquistou o bronze nos Jogos Olímpicos de Paris.",
+      link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+      tags: "judô judo judoca judoka"
+    }
+  ];
+
